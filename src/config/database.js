c2f55524fd3b319607ad9cd5b1e79e3cd85e6f9b
KhalidMujahid <binkhalid267@gmail.com>
2023-04-02 20:04:17 +0100

    Added

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 async function dbconnect() {
   mongoose.set("strictQuery", false);
-  await mongoose.connect(
-    process.env.NODE_ENV === "development"
-      ? process.env.MONGO_URI_DEV
-      : process.env.MONGO_URI
-  );
+  await mongoose.connect(process.env.MONGO_URI);
   console.log("DB connected");
 }
 
