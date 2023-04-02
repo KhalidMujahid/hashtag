187e4050b1899e80a34503224c6c2504e1f605ca
KhalidMujahid <binkhalid267@gmail.com>
2023-04-02 21:00:56 +0100

    make changes to database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function dbconnect() {
   mongoose.set("strictQuery", false);
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.NODE_ENV === "development" ? process.env.MONGO_URI : process.env.MONGO_URI_DEV);
   console.log("DB connected");
 }
 
